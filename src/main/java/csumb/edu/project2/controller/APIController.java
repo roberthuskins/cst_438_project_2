86b7c138a30b975b615176ce8eb9a1d3e0750695
Robert Huskins <robertchuskins@gmail.com>
2021-09-30 15:29:14 -0700

    all routes required in project 2 prompt. dummy data or a confirmation sentence

--- src/main/java/csumb/edu/project2/controller/APIController.java
+++ src/main/java/csumb/edu/project2/controller/APIController.java
@@ -8,63 +8,70 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Class that handles all the REST API routes
  */
 @RestController
 public class APIController {
-    //https://github.com/rajendrach/restdemo/blob/master/src/main/java/com/example/demo/controller/RestDemoController.jav
 
     @PutMapping("/newUser")
-    public void newUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-
+    public String newUser(@RequestParam String username, @RequestParam String password) {
+        return "new user added";
     }
 
     @PostMapping("/login")
-    public void login(@RequestParam("username") String username, @RequestParam("password") String password) {
-
+    public String login(@RequestParam String username, @RequestParam String password) {
+        return "login successful";
     }
 
     @PostMapping("/logout")
-    public void logout(@RequestParam("username") String username) {
-
+    public String logout(@RequestParam String username) {
+        return "logout with just username successful";
     }
 
     @DeleteMapping("/logout")
-    public void deleteUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-
+    public String deleteUser(@RequestParam String username, @RequestParam String password) {
+        return "logout with username and password (delete user) successful";
     }
 
-    //should return all items to a logged in user if they're logged in and don't specify a search
+    //If no params, then they should show all items for a specific user that is logged in. If search it should search the db. if list it will return all the items in said wishlist.
     @GetMapping("/items")
-    public List<Item> items(@RequestParam("search") String search) {
+    public List<Item> items(@RequestParam Optional<String> search, @RequestParam Optional<String> list) {
         return Arrays.asList(new Item(10.00, "airpods", "item1", "image1"), new Item(10.00, "airpods", "item1", "image1"));
-    }
-
-    //this endpoint may be combined with the one above it
-    @GetMapping("/wishlist")
-    public List<WishList> getWishlist(@RequestParam("list") String list) {
-        return Arrays.asList(new WishList("guillermo@guillermo.com", new ArrayList<>()));
     }
 
     //add item
     @PostMapping("/items")
-    public void addItem(@RequestParam("item") String list, @RequestParam("url") String url, @RequestParam("imageurl") String imageurl) {
+    public void addItem(@RequestParam String item_name, @RequestParam Optional<String> list, @RequestParam Optional<String> url, @RequestParam Optional<String> imageurl) {
+
     }
 
     @DeleteMapping("/items")
-    public void deleteItem(@RequestParam("item") String item) {
+    public String deleteItem(@RequestParam String item_name) {
+        return "item deleted";
     }
 
     @PatchMapping("/items")
-    public void updateItems(@RequestParam("item") String item) {
-
+    public String updateItems(@RequestParam String item_name) {
+        return "item added";
     }
 
-    /* Admin stuff goes below here */
+    /* Admin endpoints go below here */
+
     @GetMapping("/users")
     public List<User> users() {
         return Arrays.asList(new User("guillermo@guillermo.com", "long"), new User("rob@rob.com", "long"));
+    }
+
+    @PutMapping("/users")
+    public String createUser(@RequestParam String username, @RequestParam String password) {
+        return "user created";
+    }
+
+    @PatchMapping
+    public String updateUser(@RequestParam String username, @RequestParam String password) {
+        return "user updated";
     }
 }

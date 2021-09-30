729edbf32fbb3af61faa2653d1db4f3d93a0cbcc
arsalaerfan <erfanarsala831@gmail.com>
2021-09-30 02:30:31 -0700

    Getting sessions to work but getting connection errors

--- src/main/java/csumb/edu/project2/Project2Application.java
+++ src/main/java/csumb/edu/project2/Project2Application.java
@@ -6,10 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication
+//public class Project2Application {
+//	public static void main(String[] args) {
+//		SpringApplication.run(Project2Application.class, args);
+//	}
+//
+//}
+
 @SpringBootApplication
 public class Project2Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
 	}
-
 }
+
--- src/main/java/csumb/edu/project2/controller/controller.java
+++ src/main/java/csumb/edu/project2/controller/controller.java
@@ -6,39 +6,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-@Controller
-public class controller {
+//@Controller
+//public class controller {
+//
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+//
+//    @RequestMapping("/register")
+//    public String register(){
+//        return "register";
+//    }
+//
+//    @RequestMapping("/signin")
+//    public String login(){
+//        return "login";
+//    }
+//
+//
+//
+////    @PostMapping("/createUser")
+////    public String postExample(@RequestBody User user) throws InterruptedException, ExecutionException {
+////        return "Created User " + user.getUsername();
+////    }
+////
+////    @PutMapping("/updateUser")
+////    public String putExample(@RequestBody User user) throws InterruptedException, ExecutionException {
+////        return "Updated User" + user.getUsername();
+////    }
+////
+////    @DeleteMapping("/deleteUser")
+////    public String deleteExample(@RequestHeader String name) {
+////        return "Deleted User " + name;
+//    }
+//
+//}
 
-    @RequestMapping("/")
-    public String index() {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class controller{
+
+    @GetMapping("/")
+    public String process(Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+        model.addAttribute("sessionMessages", messages);
+
         return "index";
     }
 
-    @RequestMapping("/register")
-    public String register(){
-        return "register";
+    @PostMapping("/persistMessage")
+    public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        }
+        messages.add(msg);
+        request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        return "redirect:/";
     }
 
-    @RequestMapping("/signin")
-    public String login(){
-        return "login";
+    @PostMapping("/destroy")
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/";
     }
-
-    @PostMapping("/createUser")
-    public String postExample(@RequestBody User user) throws InterruptedException, ExecutionException {
-        return "Created User " + user.getUsername();
-    }
-
-    @PutMapping("/updateUser")
-    public String putExample(@RequestBody User user) throws InterruptedException, ExecutionException {
-        return "Updated User" + user.getUsername();
-    }
-
-    @DeleteMapping("/deleteUser")
-    public String deleteExample(@RequestHeader String name) {
-        return "Deleted User " + name;
-    }
-
 }

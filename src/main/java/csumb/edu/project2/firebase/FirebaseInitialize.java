8709dde6e8438a21d4a2abf88ba43341b9c88f8a
Robert Huskins <37780741+roberthuskins@users.noreply.github.com>
2021-10-08 12:09:54 -0700

    Set Up Heroku (#41)
    The challenge here was getting in our Firebase key, as the only way to do it is by reading from a json file. We did it by reading a bunch of environment variables and then combining them into a stream.

--- src/main/java/csumb/edu/project2/firebase/FirebaseInitialize.java
+++ src/main/java/csumb/edu/project2/firebase/FirebaseInitialize.java
@@ -1,12 +1,15 @@
 package csumb.edu.project2.firebase;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Service;
+import org.apache.commons.io.IOUtils;
+
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.*;
 
 /***
  * This class initializes the connection to Firestore. It is run automatically because of the @PostConstruct method.
@@ -14,21 +17,56 @@ import java.io.FileInputStream;
  */
 @Service
 public class FirebaseInitialize {
-
     @PostConstruct
     public void initialize() {
-        try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("./laundrylist-687e2-firebase-adminsdk-staqg-86c89737bc.json");
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://laundrylist-687e2-default-rtdb.firebaseio.com")
-                    .build();
+        if (System.getenv("FIREBASE_TYPE") == null) {
+            try {
 
-            FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            e.printStackTrace();
+                FileInputStream serviceAccount =
+                        new FileInputStream("./laundrylist-687e2-firebase-adminsdk-staqg-86c89737bc.json");
+
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://laundrylist-687e2-default-rtdb.firebaseio.com")
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(createFirebaseCredential()))
+                        .setDatabaseUrl("https://laundrylist-687e2-default-rtdb.firebaseio.com")
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private InputStream createFirebaseCredential() throws Exception {
+        String privateKey = System.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
+
+        FirebaseCredential firebaseCredential = new FirebaseCredential();
+        firebaseCredential.setType(System.getenv("FIREBASE_TYPE"));
+        firebaseCredential.setProject_id(System.getenv("FIREBASE_PROJECT_ID"));
+        firebaseCredential.setPrivate_key_id("FIREBASE_PRIVATE_KEY_ID");
+        firebaseCredential.setPrivate_key(privateKey);
+        firebaseCredential.setClient_email(System.getenv("FIREBASE_CLIENT_EMAIL"));
+        firebaseCredential.setClient_id(System.getenv("FIREBASE_CLIENT_ID"));
+        firebaseCredential.setAuth_uri(System.getenv("FIREBASE_AUTH_URI"));
+        firebaseCredential.setToken_uri(System.getenv("FIREBASE_TOKEN_URI"));
+        firebaseCredential.setAuth_provider_x509_cert_url(System.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"));
+        firebaseCredential.setClient_x509_cert_url(System.getenv("FIREBASE_CLIENT_X509_CERT_URL"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(firebaseCredential);
+
+        return IOUtils.toInputStream(jsonString);
     }
 }
